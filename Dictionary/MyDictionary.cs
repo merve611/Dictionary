7b6ab42f7456c462a43dc909a06644a2b9970fd2
merve <mrveeckrrr@hotmail.com>
2021-02-03 19:11:16 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] Keys;
+        TValue[] Values;
+
+        //Constructor;
+        public MyDictionary()
+        {
+            Keys = new TKey[0];
+            Values = new TValue[0];
+        }
+        public void Add(TKey Key, TValue Value)
+        {
+            TKey[] TempKey = Keys;
+            TValue[] TempValue = Values;
+
+            Keys = new TKey[Keys.Length + 1];
+            Values = new TValue[Values.Length + 1];
+
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+                Keys[i] = TempKey[i];
+                Values[i] = TempValue[i];
+            }
+
+            Keys[Keys.Length - 1] = Key;
+            Values[Values.Length - 1] = Value;
+        }
+
+
+
+        //public int Length
+        //{
+        //    get { return Keys.Length; }
+
+        //}
+
+        //public TKey[] keys
+        //{
+        //    get { return Keys; }
+        //}
+
+
+        //public TValue[] values
+        //{
+        //    get { return Values; }
+
+        //}
+
+
+
+        public void List()
+
+        {
+            for (int i = 0; i < Keys.Length; i++)
+            {
+                Console.WriteLine(Keys[i] + "  " + Values[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(61, "Trabzon");
+            sehirler.Add(69, "Bayburt");
+            sehirler.Add(55, "Samsun");
+            sehirler.List();
+        }
+    }
+
+
+
+}
